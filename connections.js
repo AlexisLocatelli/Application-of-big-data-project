3d1f924b28e810845bc12663988ec07ba1f47d6d
AlexisLocatelli <74095649+AlexisLocatelli@users.noreply.github.com>
2023-12-08 18:38:00 +0100

    Modification du projet
    Modification de la connecion a la base de donnée, modification des fonction post et update

--- connections.js
+++ connections.js
@@ -1,57 +1,54 @@
 const mongoose = require('mongoose');
 
-// Connexion à la base de données MongoDB (assurez-vous d'avoir MongoDB installé localement)
-mongoose.connect('mongodb+srv://alexisLocatelli:ArZnfglk7nhG2IHa@enterprisecluster.ism0vxp.mongodb.net/Enterprise');
+mongoose.connect('mongodb://127.0.0.1:27017/Enterprise');
 const db = mongoose.connection;
 
-// Gestion des erreurs de connexion à la base de données
 db.on('error', console.error.bind(console, 'Erreur de connexion à la base de données :'));
-db.once('open', () => {
+db.once('open', async () => {
   console.log('Connexion réussie à la base de données');
 });
 
-// Définir un schéma pour le modèle d'entreprise
 const enterpriseSchema = new mongoose.Schema({
-  siren : String,
-  nic : String,
-  siret : String,
+  siren : Number,
+  nic : Number,
+  siret : Number,
   statutDiffusionEtablissement : String,
-  dateCreationEtablissement : String,
+  dateCreationEtablissement : Date,
   trancheEffectifsEtablissement : String,
-  anneeEffectifsEtablissement : String,
+  anneeEffectifsEtablissement : Number,
   activitePrincipaleRegistreMetiersEtablissement : String,
-  dateDernierTraitementEtablissement : String,
-  etablissementSiege : String,
-  nombrePeriodesEtablissement : String,
+  dateDernierTraitementEtablissement : Date,
+  etablissementSiege : Boolean,
+  nombrePeriodesEtablissement : Number,
   complementAdresseEtablissement : String,
   numeroVoieEtablissement : String,
   indiceRepetitionEtablissement : String,
   typeVoieEtablissement : String,
   libelleVoieEtablissement : String,
-  codePostalEtablissement : String,
+  codePostalEtablissement : Number,
   libelleCommuneEtablissement : String,
   libelleCommuneEtrangerEtablissement : String,
   distributionSpecialeEtablissement : String,
-  codeCommuneEtablissement : String,
-  codeCedexEtablissement : String,
+  codeCommuneEtablissement : Number,
+  codeCedexEtablissement : Number,
   libelleCedexEtablissement : String,
   codePaysEtrangerEtablissement : String,
   libellePaysEtrangerEtablissement : String,
   complementAdresse2Etablissement : String,
-  numeroVoie2Etablissement : String,
+  numeroVoie2Etablissement : Number,
   indiceRepetition2Etablissement : String,
   typeVoie2Etablissement : String,
   libelleVoie2Etablissement : String,
-  codePostal2Etablissement : String,
+  codePostal2Etablissement : Number,
   libelleCommune2Etablissement : String,
   libelleCommuneEtranger2Etablissement : String,
   distributionSpeciale2Etablissement : String,
-  codeCommune2Etablissement : String,
+  codeCommune2Etablissement : Number,
   codeCedex2Etablissement : String,
   libelleCedex2Etablissement : String,
   codePaysEtranger2Etablissement : String,
   libellePaysEtranger2Etablissement : String,
-  dateDebut : String,
+  dateDebut : Date,
   etatAdministratifEtablissement : String,
   enseigne1Etablissement : String,
   enseigne2Etablissement : String,
@@ -61,27 +58,8 @@ const enterpriseSchema = new mongoose.Schema({
   nomenclatureActivitePrincipaleEtablissement : String,
   caractereEmployeurEtablissement : String,
 });
-
-// Créer un modèle d'entreprise à partir du schéma
 const Enterprise = mongoose.model('enterprises', enterpriseSchema);
 
-async function get_enterprise(siret) {
-  const enterprise = await Enterprise.find({siret:siret}).exec();
-  return enterprise
-}
-
-async function delete_enterprise(siret) {
-  const enterprise = await Enterprise.find({siret:siret}).exec();
-  enterprise.deleteone();
-}
-
-async function post_enterprise(enterprise) {
-  const newEnterprise = new Enterprise(req.body);
-  await newEnterprise.save();
-}
-
 module.exports = {
-    get_enterprise,
-    post_enterprise,
-    delete_enterprise
+    Enterprise
 }
